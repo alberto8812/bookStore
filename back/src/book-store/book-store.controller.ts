9fb831d619d8f738f068f88ff3e198f7e8651e57
Carlos8992 <cvelasco@saludelectronica.com>
2026-03-21 00:13:05 -0500

    feat: agregar endpoint para listar libros con paginación

--- back/src/book-store/book-store.controller.ts
+++ back/src/book-store/book-store.controller.ts
@@ -2,18 +2,25 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { BookStoreService } from './book-store.service';
 import { CreateBookStoreDto } from './dto/create-book-store.dto';
 import { UpdateBookStoreDto } from './dto/update-book-store.dto';
+import { Endpoint } from 'src/shared/decorator/endpoint.decorator';
+import { PaginationDto } from 'src/shared/dto/pagination.dto';
 
 @Controller('book-store')
 export class BookStoreController {
-  constructor(private readonly bookStoreService: BookStoreService) {}
+  constructor(private readonly bookStoreService: BookStoreService) { }
 
   @Post()
   create(@Body() createBookStoreDto: CreateBookStoreDto) {
     return this.bookStoreService.create(createBookStoreDto);
   }
 
-  @Get()
-  findAll() {
+  @Endpoint({
+    method: 'POST',
+    summary: 'List books with pagination',
+    route: 'pagination',
+    responses: [{ status: 200, description: 'Paginated list', type: Object }],
+  })
+  findAll(@Body() paginationDto: PaginationDto) {
     return this.bookStoreService.findAll();
   }
 
