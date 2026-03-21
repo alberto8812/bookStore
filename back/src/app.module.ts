9c8fb595a762cc655625759f231c2beba051eac9
Carlos8992 <cvelasco@saludelectronica.com>
2026-03-21 00:29:21 -0500

    feat: agregar módulo de autenticación con controladores y servicios para gestión de usuarios

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { BookStoreModule } from './book-store/book-store.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BookStoreModule],
+  imports: [BookStoreModule, AuthModule],
   controllers: [],
   providers: [],
 })
